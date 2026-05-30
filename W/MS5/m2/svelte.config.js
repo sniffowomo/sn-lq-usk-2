31d735162d499b66071da7806821b65e01120f8c
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2026-05-30 17:59:51 +0000

    🍫 MS5 - uw

--- W/MS5/m2/svelte.config.js
+++ W/MS5/m2/svelte.config.js
@@ -1,5 +1,4 @@
-// import cloudflare from "@sveltejs/adapter-cloudflare";
-import vercel from '@sveltejs/adapter-vercel'
+import ada from '@sveltejs/adapter-auto'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,7 +11,7 @@ const config = {
     // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-    adapter: vercel(),
+    adapter: ada(),
   },
 }
 
