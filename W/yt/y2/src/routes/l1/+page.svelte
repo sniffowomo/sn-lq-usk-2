d4e41dbdfa5ece627134f6ca7a1779ae58b405a0
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2026-05-31 17:25:54 +0000

    🫧 yt

--- W/yt/y2/src/routes/l1/+page.svelte
+++ W/yt/y2/src/routes/l1/+page.svelte
@@ -7,6 +7,16 @@
 
   //    Work zone
   let title = 'Panty'
+  let src_i1 =
+    'https://res.cloudinary.com/dsec3pqkf/image/upload/v1780247397/giphy_zpuzto.gif'
+  let alt_i1 = 'booty'
+
+  // Spread Syntax
+  let img_props_method = {
+    src: 'https://res.cloudinary.com/dsec3pqkf/image/upload/v1780247397/giphy_zpuzto.gif',
+    alt: 'pantybooty',
+    width: '300',
+  }
 </script>
 
 <div class="flex">
@@ -33,6 +43,18 @@
   <Wvd2 />
 
   <!-- Work zone -->
-  <p>Testing the templating</p>
+  <p class="p2">Testing the templating</p>
   <h1>{title}</h1>
+
+  <p class="p2">
+    Testing standard image tags, note in this case we are using variable tags,
+    that have been defined in the script section
+  </p>
+  <img src={src_i1} alt={alt_i1} width="200" />
+
+  <p class="p2">
+    Note below using the image props method , this is so much better, to reduce
+    unncessary code
+  </p>
+  <img {...img_props_method} />
 </div>
