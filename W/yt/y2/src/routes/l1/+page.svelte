ccdc433cd33ee5d78a2148e1fd13438c639a51d7
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2026-05-31 17:05:56 +0000

    🫧 yt

--- W/yt/y2/src/routes/l1/+page.svelte
+++ W/yt/y2/src/routes/l1/+page.svelte
@@ -1,13 +1,38 @@
 <!-- This is the lesson1 route-->
 
 <script>
+  // --- Imoports for lines ---
+  import Wvd2 from '$lib/panty/WaveDiv2.svelte'
   import Wvd3 from '$lib/panty/WaveDiv3.svelte'
+
+  //    Work zone
+  let title = 'Panty'
 </script>
 
 <div class="flex">
-  <Wvd3 />
+  <!-- nav zone -->
+  <Wvd3
+    thickness={1}
+    bumpThickness={2}
+    bumpWidth={240}
+    glow={24}
+    speed={6}
+    breatheSpeed={12.5}
+  />
   <a class="btn" href="/"> Home </a>
-  <Wvd3 />
+  <Wvd3
+    thickness={1}
+    bumpThickness={2}
+    bumpWidth={240}
+    glow={24}
+    speed={6}
+    breatheSpeed={2.5}
+  />
 
   <h1>Lesson 1 - Route</h1>
+  <Wvd2 />
+
+  <!-- Work zone -->
+  <p>Testing the templating</p>
+  <h1>{title}</h1>
 </div>
