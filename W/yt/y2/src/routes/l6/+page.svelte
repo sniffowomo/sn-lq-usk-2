d84e1c171279b330bcb069b83363ec2fa575aacd
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2026-06-01 18:25:19 +0000

    🫧 yt

--- W/yt/y2/src/routes/l6/+page.svelte
+++ W/yt/y2/src/routes/l6/+page.svelte
@@ -3,7 +3,17 @@
 <script>
   // --- Imoports for lines ---
   import Navz from '$lib/co/Navz.svelte'
+  import Wvd1 from '$lib/panty/WavDiv.svelte'
   import Wvd2 from '$lib/panty/WaveDiv2.svelte'
+
+  let count = $state(0)
+  let condition = $state(false)
+
+  $effect(() => {
+    if (condition) {
+      console.log('Count is going to be tracked', count)
+    }
+  })
 </script>
 
 <div class="flex">
@@ -15,6 +25,15 @@
     Working with <code>$effect()</code> rune which is supposed to be ane escape hatch
   </p>
   <Wvd2 />
+
+  <div class="flex2">
+    <button class="btn" onclick={() => count++}> Buto </button>
+    <button class="btn" onclick={() => (condition = !condition)}>
+      Condition
+    </button>
+    <p>Conditiion = {condition}</p>
+  </div>
+  <Wvd1 />
 
   <!-- ////// Main Body End Tag ///// -->
 </div>
