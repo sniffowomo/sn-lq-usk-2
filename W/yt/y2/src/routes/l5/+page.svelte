6207d67375c926fa4ed2bd147881a65994248b5c
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2026-06-01 17:37:03 +0000

    🫧 yt

--- W/yt/y2/src/routes/l5/+page.svelte
+++ W/yt/y2/src/routes/l5/+page.svelte
@@ -4,6 +4,19 @@
   // --- Imoports for lines ---
   import Navz from '$lib/co/Navz.svelte'
   import Wvd2 from '$lib/panty/WaveDiv2.svelte'
+
+  // Shopping cart Example
+  let cart = $state([
+    { item: '🍎', tt: 20 },
+    { item: '🍌', tt: 10 },
+  ])
+  let total = $derived.by(() => {
+    let sum = 0
+    for (let item of cart) {
+      sum += item.tt
+    }
+    return sum
+  })
 </script>
 
 <div class="flex">
@@ -15,6 +28,20 @@
     More Work with <code>$derived()</code> & <code>$state()</code>
   </p>
   <Wvd2 />
+
+  <p class="p2">Working with arrays and getting total</p>
+
+  <!-- Code for looping through array and printing -->
+  <ul>
+    {#each cart as i}
+      <li>{i.item} has {i.tt}</li>
+    {/each}
+  </ul>
+
+  <p>Total from Array: {total}</p>
+  <Wvd2 />
+
+  <p class="p2">Side Effects Rune</p>
 
   <!-- ////// Main Body End Tag ///// -->
 </div>
