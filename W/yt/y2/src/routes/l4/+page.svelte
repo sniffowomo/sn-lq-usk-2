f06a2dfe92c16b7a0ff3de67522261dcde7a5095
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2026-06-01 15:19:58 +0000

    🫧 yt

--- W/yt/y2/src/routes/l4/+page.svelte
+++ W/yt/y2/src/routes/l4/+page.svelte
@@ -9,19 +9,44 @@
   let count = $state(0)
   let factor = $state(0)
   let result = $derived(count * factor)
+
+  let count2 = $state(0)
+  let max = $derived(count2 >= 4)
+  $inspect(max)
+
+  // More work on derived
+  let count3 = $state(0)
+  let max2 = $derived(limit())
+
+  function limit() {
+    return count > 4
+  }
 </script>
 
 <div class="flex">
+  <!-- ////// Main Start Tag ///// -->
   <Navz />
 
   <h1>Lesson 4 - More Reactivity</h1>
   <p class="p2">Adding more work on top of l3</p>
   <Wvd2 />
 
+  <!-- Testing Derived values here -->
   <p class="p2">
     Below we will multipled the states , the 'results' is the derived
   </p>
-  <p style="font-size: 2rem">Result is {count} * {factor} = {result}</p>
-  <button class="btn" onclick={() => count++}>Count: {count}</button>
-  <button class="btn" onclick={() => factor++}>Factor: {factor}</button>
+  <div class="flex2">
+    <p style="font-size: 2rem">Result is {count} * {factor} = {result}</p>
+    <button class="btn" onclick={() => count++}>Count: {count}</button>
+    <button class="btn" onclick={() => factor++}>Factor: {factor}</button>
+  </div>
+
+  <Wvd2 />
+  <!-- Testing Ley Loading here  -->
+  <p class="p2">Derived lazy Loading testing here</p>
+  <button class="btn" onclick={() => count2++} disabled={max}
+    >Count: {count2}</button
+  >
+
+  <!-- ////// Main Body End Tag ///// -->
 </div>
