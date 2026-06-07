49e7022c1519fc8dd23f70a2c9eb7e459d59a6fb
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2026-06-07 05:58:06 +0000

    🫧 yt

--- W/yt/y22/src/routes/p1/+page.svelte
+++ W/yt/y22/src/routes/p1/+page.svelte
@@ -1,8 +1,48 @@
+<script>
+  let count = $state(0)
+
+  let codeContent = `
+  <code> Code Content Test </code>
+  `
+
+  // Section on editor state
+  let editor = $state({
+    content: `BotySniff`,
+  })
+</script>
+
 <main>
+  <!-- //// Starting tag dont touch //// -->
+  <!-- //// Starting tag dont touch //// -->
+  <!-- //// Starting tag dont touch //// -->
   <div class="button-group">
     <a href="/" class="link-btn">Home </a>
+    <h1>Lesson 1 Work - Focus on reactivity</h1>
     <div class="tech-divider"></div>
   </div>
 
-  <h1>Lesson 1 Work - Focus on reactivity</h1>
+  <div class="glass-card">
+    <h3 style:padding="0.2rem">But with onclick listener and own function</h3>
+    <button class="nav-btn" onclick={() => count++}>
+      Count: {count}
+    </button>
+    <p>This main</p>
+    {@html codeContent}
+  </div>
+  <!-- Text area section test -->
+  <div class="glass-card">
+    <h3 style:padding="0.2rem">Text Area Input</h3>
+    textarea
+  </div>
+
+  <!-- //// Ending tag dont touch //// -->
+  <!-- //// Ending tag dont touch //// -->
+  <!-- //// Ending tag dont touch //// -->
 </main>
+
+<style>
+  .nav-btn {
+    padding: 1.2rem;
+    font-size: 2rem;
+  }
+</style>
