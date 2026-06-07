992b237d00c21fd18f11e6473e6a6724ce207bbb
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2026-06-07 16:52:05 +0000

    🫧 yt

--- W/yt/y22/src/routes/p2/+page.svelte
+++ W/yt/y22/src/routes/p2/+page.svelte
@@ -1,5 +1,16 @@
 <script>
+  // Effects1
   let count = $state(0)
+  let condition = $state(false)
+
+  $effect(() => {
+    // track count only when condition is true , the condition is being changed with the button via onlick
+    if (condition) {
+      console.log(count)
+    }
+  })
+
+  // Effect edge case using the interval example
 </script>
 
 <main>
@@ -12,7 +23,17 @@
 
   <div class="glass-card">
     <h3>Effects Study 1</h3>
+
     <button class="nav-btn" onclick={() => count++}>Cliq : {count}</button>
+
+    <p style:padding="2rem">Below is button is for testing condition</p>
+    <button class="nav-btn" onclick={() => (condition = !condition)}
+      >Conditon Toggle : {condition}</button
+    >
+  </div>
+
+  <div class="glass-card">
+    <h3>Effects Edge Case Study</h3>
   </div>
 
   <!-- //// Ending tag dont touch //// -->
