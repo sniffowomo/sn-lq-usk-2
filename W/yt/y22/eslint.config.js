59528696da21026fe6b8f4a0d1e0e9ad10d2d24b
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2026-06-07 04:58:25 +0000

    🫧 yt

--- W/yt/y22/eslint.config.js
+++ W/yt/y22/eslint.config.js
@@ -1,45 +1,32 @@
-mport svelteConfig from './svelte.config.js';
-import { defineConfig } from 'eslint/config';
-import globals from 'globals';
-import js from '@eslint/js';
-import svelte from 'eslint-plugin-svelte';
+import svelteConfig from './svelte.config.js'
+import globals from 'globals'
+import js from '@eslint/js'
+import svelte from 'eslint-plugin-svelte'
 
-export default defineConfig([
-  // ...
+export default [
   js.configs.recommended,
-  svelte.configs.recommended,
+  ...svelte.configs.recommended,
   {
     languageOptions: {
       globals: {
         ...globals.browser,
-        // for Sveltekit in non-SPA mode
-        ...globals.node
-      }
-    }
+        ...globals.node,
+      },
+    },
   },
   {
     files: ['**/*.svelte', '**/*.svelte.js'],
     languageOptions: {
       parserOptions: {
-        // explicitly importing allows for better compatibilty and functionality with rules and other tooling that depend on the config file.
-        //
-        // Note: `eslint --cache` will fail with non-serializable properties.
-        // In those cases, please remove the non-serializable properties.
-        // svelteConfig: {
-        //   ...svelteConfig,
-        //   kit: {
-        //     ...svelteConfig.kit,
-        //     typescript: undefined
-        //   }
-        // }
-        svelteConfig
-      }
-    }
+        svelteConfig,
+      },
+    },
   },
   {
     rules: {
-      // Override or add rule settings here, such as:
-      // 'svelte/rule-name': 'error'
-    }
-  }
-]);
+      'svelte/no-unused-svelte-ignore': 'warn',
+      'svelte/valid-compile': 'error',
+      'svelte/no-navigation-without-resolve': 'warn',
+    },
+  },
+]
