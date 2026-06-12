a7c0b0b91b2230b171390659a2303389b0fbdc5a
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2026-06-12 14:31:36 +0000

    🫧 yt

--- W/yt/y22/src/routes/p3/+page.svelte
+++ W/yt/y22/src/routes/p3/+page.svelte
@@ -1,3 +1,12 @@
+<!-- 
+ Using effects by querying api
+-->
+
+<script>
+  let pokemon = $state('baguna')
+  let image = $state('')
+</script>
+
 <main>
   <!-- //// Starting tag dont touch //// -->
   <!-- //// Starting tag dont touch //// -->
@@ -5,6 +14,22 @@
 
   <h1>Effects Study 2</h1>
   <div class="tech-divider"></div>
+
+  <div class="glass-card">
+    <h3>Pokemon Search function</h3>
+
+    <!-- Input Search -->
+    <div class="stack">
+      <input
+        type="search"
+        class="input-glass"
+        placeholder="Enter PantyMon Name"
+        oninput={(e) => (pokemon = e.target.value)}
+      />
+
+      <img src={image} alt="PantyMon" />
+    </div>
+  </div>
 
   <!-- //// Ending tag dont touch //// -->
   <!-- //// Ending tag dont touch //// -->
