2c6c32c5ec9e98e3d76833095897387e605bee31
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2026-06-14 15:25:38 +0000

    🫧 yt

--- W/yt/y22/src/routes/p3/+page.svelte
+++ W/yt/y22/src/routes/p3/+page.svelte
@@ -45,8 +45,20 @@
   let items = $state([...Array(10).keys()])
 
   function shuffle() {
-    items = items.sort(() => Math.random() - 0.5)
+    items = items.toSorted(() => Math.random() - 0.5)
   }
+
+  $effect.pre(() => {
+    items
+    const state = Flip.getState('.flipboard-numbers > span')
+    queueMicrotask(() => {
+      Flip.from(state, {
+        duration: 1,
+        stagger: 0.01,
+        ease: 'power1.inOut',
+      })
+    })
+  })
 </script>
 
 <main>
@@ -57,6 +69,29 @@
   <h1>Effects Study 2</h1>
   <div class="tech-divider"></div>
 
+  <!-- Gsap animation flip here -->
+  <div class="glass-card">
+    <h3>
+      <code>effects.pre</code> Need to do something before the dom updates
+    </h3>
+
+    <div class="stack">
+      <div class="flipboard-numbers">
+        {#each items as item (item)}
+          <span>{item}</span>
+        {/each}
+      </div>
+      <button
+        class="nav-btn"
+        onclick={shuffle}
+        style="padding: 2rem; font-size: 2rem; margin-top: 1rem; border-radius: 99px;"
+      >
+        Shuffle</button
+      >
+    </div>
+  </div>
+
+  <!-- Fetching and effects of pokemon -->
   <div class="glass-card">
     <h3>Pokemon Search function</h3>
     <p style:padding="1rem">
@@ -80,30 +115,10 @@
     </div>
   </div>
 
+  <!-- Testing you own fetch gif here -->
   <div class="glass-card">
     <h3>Testing My own Api Call method</h3>
     <Plic {form} />
-  </div>
-
-  <div class="glass-card">
-    <h3>
-      <code>effects.pre</code> Need to do something before the dom updates
-    </h3>
-
-    <div class="stack">
-      <div class="flipboard-numbers">
-        {#each items as item (item)}
-          <span>{item}</span>
-        {/each}
-      </div>
-      <button
-        class="nav-btn"
-        onclick={shuffle}
-        style="padding: 2rem; font-size: 2rem; margin-top: 1rem; border-radius: 99px;"
-      >
-        Shuffle</button
-      >
-    </div>
   </div>
 
   <!-- //// Ending tag dont touch //// -->
